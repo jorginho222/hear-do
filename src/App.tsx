f28fa2971d9877c31ccd090c63820b71da0fdb59
jorginho222 <rodizioperalta22@gmail.com>
2024-05-14 15:10:09 -0300

    Select sounds to play randomly

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import './App.css'
-import {Button, List, ListItemText, ListSubheader, Stack, Switch} from "@mui/material";
-import {useEffect, useRef, useState} from "react";
+import {Box, Button, ListItemText, Stack, ToggleButton, ToggleButtonGroup} from "@mui/material";
+import React, {useEffect, useRef, useState} from "react";
+import LaptopIcon from '@mui/icons-material/Laptop';
 
 function App() {
     const [playing, setPlaying] = useState(false)
-    const audiosList = useRef([])
-    const audio = new Audio('/Glockenspiel.mp3');
-    const intervalRef = useRef(setInterval(() => {
-    }, 0))
+    const audiosList: Array<{ key: string, name: string, src: string }> = useRef([
+        {key: 'glock', name: 'Glockenspiel1', src: '/Glockenspiel.mp3'},
+        {key: 'glock2', name: 'Glockenspiel2', src: '/Glockenspiel.mp3'},
+        {key: 'glock3', name: 'Glockenspiel3', src: '/Glockenspiel.mp3'},
+    ])
+    const [selectedAudios, setSelectedAudios] = useState(['glock'])
+    const intervalRef = useRef(setInterval(() => {}, 0))
 
-    // audio.load();
+    const handleSelectedAudios = (event: React.MouseEvent<HTMLElement>, newAudios: Array<string>) => {
+            setSelectedAudios(newAudios)
+    }
+
+    // audioPath.load();
     useEffect(() => {
         clearInterval(intervalRef.current);
     }, [])
@@ -17,8 +25,13 @@ function App() {
     const play = () => {
         setPlaying(true)
         intervalRef.current = setInterval(() => {
-            audio.play();
-        }, (Math.floor(Math.random() * 10) + 1) * 1000);
+            const randomSelected: string = selectedAudios[Math.floor(Math.random() * selectedAudios.length)]
+            const randomAudio = audiosList.current.find((audio: { key: string, name: string, src: string }) => audio.key === randomSelected)
+            const randomAudioInstance = new Audio(randomAudio.src)
+
+            console.log(randomSelected)
+            randomAudioInstance.play();
+        }, (Math.floor(Math.random() * 6) + 1) * 1000);
     }
 
     const stop = () => {
@@ -28,37 +41,37 @@ function App() {
 
     return (
         <>
-            <List
-                sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}
-                component="nav"
-                aria-labelledby="nested-list-subheader"
-                subheader={
-                    <ListSubheader component="div" id="nested-list-subheader">
-                        Sonidos
-                    </ListSubheader>
-                }
+            <ToggleButtonGroup
+                orientation="vertical"
+                value={selectedAudios}
+                onChange={handleSelectedAudios}
+                aria-label="audio"
             >
-                <ListItemText primary="Glockenspiel.mp3" />
-                <Switch />
-                <ListItemText primary="Glockenspiel.mp3" />
-            </List>
-
-            <Stack direction="row" spacing={2}>
-                <Button
-                    color="success"
-                    variant="contained"
-                    onClick={play}
-                >
-                    Play
-                </Button>
-                <Button
-                    color="error"
-                    variant="contained"
-                    onClick={stop}
-                >
-                    Stop
-                </Button>
-            </Stack>
+                {audiosList.current.map((audio: { key: string, name: string, src: string }, index: number) => (
+                    <ToggleButton key={index} value={audio.key} sx={{marginTop: '5px'}}>
+                        <LaptopIcon/>
+                        <ListItemText primary={audio.name}/>
+                    </ToggleButton>
+                ))}
+            </ToggleButtonGroup>
+            <Box sx={{marginTop: '20px'}}>
+                <Stack direction="row" spacing={2}>
+                    <Button
+                        color="success" variant="contained"
+                        disabled={playing || selectedAudios.length === 0}
+                        onClick={play}
+                    >
+                        Play
+                    </Button>
+                    <Button
+                        color="error" variant="contained"
+                        disabled={!playing}
+                        onClick={stop}
+                    >
+                        Stop
+                    </Button>
+                </Stack>
+            </Box>
         </>
     )
 }
